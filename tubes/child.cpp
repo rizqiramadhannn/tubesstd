e19955795e1df08dada9249b5cc0cdd58daa8d1d
chara-unch <62688600+chara-unch@users.noreply.github.com>
2020-03-28 12:43:48 +0700

    Update child.cpp

--- tubes/child.cpp
+++ tubes/child.cpp
@@ -1,2 +1,71 @@
 #include "child.h"
-#define
+#include <iostream>
+
+using namespace std;
+
+address allocate(infotype x){
+    address p = new elmlist;
+    info(p) = x;
+    next(p) = NULL;
+
+    return P;
+}
+
+void insertFirst(List &L, address P){
+    next(P) = first(L);
+    first(L) = P;
+}
+
+void insertLast(List &L, address P){
+    address Q;
+    Q = first(L);
+    while(next(Q) != NULL){
+        Q = next(Q);
+    next(Q) = P;
+    }
+}
+
+void insertAfter(List &L, address Prec, address P){
+    if(Prec != NULL){
+        next(P) = next(Prec);
+        next(Prec) = P;
+    }
+}
+
+void deleteFirst(List &L, address &P){
+    if (first(L) != NULL ){
+        P = first(L);
+        first(L) = next(P);
+
+        next(P) = NULL;
+    }
+}
+void deleteLast(List &L, address &P){
+    address Q;
+    if (first(L) = NULL){
+        cout<<"Empty"<<endl;
+    } else if(next(first(L)) = NULL){
+       P =first(L);
+       first(L) = NULL;
+    } Q = first(L);
+    while (next(next(Q)) != NULL){
+        Q = next(Q);
+    next(Q) = NULL;
+    }
+
+}
+
+void deleteAfter(List &L, address Prec, address P){
+    P = next(Prec);
+    next(Prec) = next(P);
+    next(P) = NULL;
+}
+
+void printInfo(List L){
+    address p = first(L);
+    while(p != NULL){
+        cout<<info(p)<<", ";
+        p = next(p);
+    }
+    cout<<endl;
+}
