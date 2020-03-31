4f11bf63b0bd775848e351d566eaf5787e6f97e4
rizqiramadhannn <rizqiramadhannn@users.noreply.github.com>
2020-03-31 18:37:41 +0700

    Update main.cpp

--- tubes/main.cpp
+++ tubes/main.cpp
@@ -5,11 +5,45 @@ int main()
 {
     List_p Kota;
     createList(Kota);
-    insertKota(Kota);
-    insertKota(Kota);
-    printInfo(Kota);
-    insertKota(Kota);
-    printInfo(Kota);
-    insertKota(Kota);
-    printInfo(Kota);
+    string input;
+    while (input != "0"){
+        cout << "Menu" << endl;
+        cout << "1. Tambah data kota" << endl;
+        cout << "2. Tambah data jalan" << endl;
+        cout << "3. ?" << endl;
+        cout << "4. Hapus data kota" << endl;
+        cout << "5. Hapus data jalan" << endl;
+        cout << "6. Tampilkan semua data" << endl;
+        cout << "7. Tampilkan data kota berdasarkan jalan" << endl;
+        cout << "8. Tampilkan data jalan berdasarkan kota" << endl;
+        cout << "9. Tampilkan data jalan berdasarkan tipe jalan" << endl;
+        cout << "0. End Program" << endl;
+        cout << "---------------------------" << endl;
+        cout << "Pilih menu : ";
+        cin >> input;
+        cout << endl;
+        if (input == "1"){
+            insertKota(Kota);
+        } else if (input == "2"){
+            cout << "Belum kelarrr" << endl;
+        } else if (input == "3"){
+            cout << "Belum kelarrr" << endl;
+        } else if (input == "4"){
+            deleteKota(Kota);
+        } else if (input == "5"){
+            cout << "Belum kelarrr" << endl;
+        } else if (input == "6"){
+            printInfo(Kota);
+        } else if (input == "7"){
+            cout << "Belum kelarrr" << endl;
+        } else if (input == "8"){
+            cout << "Belum kelarrr" << endl;
+        } else if (input == "9"){
+            cout << "Belum kelarrr" << endl;
+        } else if (input != "0"){
+            cout << "Input salah/menu tidak tersedia" << endl;
+        }
+        cout << "---------------------------" << endl;
+        cout << endl;
+    }
 }
