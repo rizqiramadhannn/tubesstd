b27954b9ca349770db0c4cbde03c8deedac64737
rizqiramadhannn <rizqiramadhannn@users.noreply.github.com>
2020-04-06 19:15:57 +0700

    Update child.cpp

--- tubes/child.cpp
+++ tubes/child.cpp
@@ -1,127 +1,78 @@
 #include "child.h"
-
-address_c allocate(infotype_c x){
-    address_c p = new elmList_c;
-    info(p) = x;
-    next(p) = NULL;
-
-    return p;
-}
+#include "relasi.h"
 
 void createList(List_c &L){
     first(L) = NULL;
 }
 
-void insertJalan(List_c &L){
-    infotype_c x;
-    cout << "Masukkan nama jalan : ";
-    cin >> x;
+void insertJalan(List_c &L, infotype_c x, infotype_c tipe){
     address_c P = new elmList_c;
+    tipe(P) = tipe;
     info(P) = x;
     next(P) = NULL;
-    if (first(L) == NULL){
-        first(L) = P;
-        next(P) = P;
-    } else if (next(first(L)) == NULL) {
-        address_c Q = first(L);
-        if (x < info(first(L))){
+    if (first(L) == NULL || x < info(first(L))){
+        if (first(L) != NULL){
             next(P) = first(L);
-            first(L) = P;
-        }else {
-            Q = first(L);
-            while (x > info(Q)){
+        }
+        first(L) = P;
+    }else if (next(first(L)) == NULL){
+        next(first(L)) = P;
+    }else {
+        address_c Q = first(L);
+        while (x > info(next(Q)) && next(next(Q)) != NULL){
+            Q = next(Q);
+        }
+        if (next(Q) != NULL){
+            if(next(next(Q)) == NULL && info(next(Q)) < x){
                 Q = next(Q);
             }
-            next(P) = Q;
+            next(P) = next(Q);
         }
+        next(Q) = P;
     }
 }
 
-void deleteJalan(List_c &L){
+void deleteJalan(List_c &L, address_c &P){
     if(first(L) == NULL){
-        cout << "Tidak ada data" << endl;
+        cout << "Tidak ada data jalan" << endl;
     } else {
         printInfo(L);
         string hapus;
         cout << "Masukkan nama jalan yang akan dihapus : ";
         cin >> hapus;
-        address_c P;
         if (info(first(L)) == hapus){
             P = first(L);
-            first(L) = NULL;
-            if  (first(L) = next(P)){
+            if(next(first(L)) == NULL){
+                first(L) = NULL;
+            } else {
+                first(L) = next(P);
                 next(P) = NULL;
             }
         } else {
             address_c Q = first(L);
-            do {
+            while (Q != NULL && info(next(Q)) != hapus) {
                 Q = next(Q);
-            } while (Q != first(L) && info(next(Q)) != hapus);
-            if (Q == first(L)){
+            }
+            if (Q == NULL){
                 cout << "Nama jalan tidak ada dalam data" << endl;
                 cout << "Kembali ke menu" << endl;
             } else {
                 P = next(Q);
-                next(Q) = next(P);
-                next(P) = NULL;
+                if (next(Q) == NULL){
+                    next(Q) = NULL;
+                } else {
+                    next(Q) = next(P);
+                    next(P) = NULL;
+                }
+
             }
         }
     }
 }
 
-
-void insertFirst(List_c &L, address_c P){
-    next(P) = first(L);
-    first(L) = P;
-}
-
-void insertLast(List_c &L, address_c P){
-    address_c Q;
-    Q = first(L);
-    while(next(Q) != NULL){
-        Q = next(Q);
-    next(Q) = P;
-    }
-}
-
-void insertAfter(List_c &L, address_c Prec, address_c P){
-    if(Prec != NULL){
-        next(P) = next(Prec);
-        next(Prec) = P;
-    }
-}
-
-void deleteFirst(List_c &L, address_c &P){
-    if (first(L) != NULL ){
-        P = first(L);
-        first(L) = next(P);
-
-        next(P) = NULL;
-    }
-}
-void deleteLast(List_c &L, address_c &P){
-    address_c Q;
-    if (first(L) = NULL){
-        cout<<"Empty"<<endl;
-    } else if(next(first(L)) = NULL){
-       P =first(L);
-       first(L) = NULL;
-    } Q = first(L);
-    while (next(next(Q)) != NULL){
-        Q = next(Q);
-    next(Q) = NULL;
-    }
-
-}
-
-void deleteAfter(List_c &L, address_c Prec, address_c P){
-    P = next(Prec);
-    next(Prec) = next(P);
-    next(P) = NULL;
-}
-
 void printInfo(List_c L){
     address_c p = first(L);
+    cout << "Daftar Jalan : " << endl;
     while(p != NULL){
         cout<<info(p)<<", ";
         p = next(p);
