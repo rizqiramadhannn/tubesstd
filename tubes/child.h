9738429d67caced81f69b13bb0425272a3040f33
rizqiramadhannn <rizqiramadhannn@users.noreply.github.com>
2020-04-06 19:16:13 +0700

    Update child.h

--- tubes/child.h
+++ tubes/child.h
@@ -4,6 +4,7 @@
 #define first(L) L.first
 #define next(P) P->next
 #define info(P) P->info
+#define tipe(P) P->tipe
 using namespace std;
 
 typedef string infotype_c;
@@ -11,6 +12,7 @@ typedef struct elmList_c *address_c;
 
 struct elmList_c{
     infotype_c info;
+    infotype_c tipe;
     address_c next;
 };
 
@@ -18,16 +20,9 @@ struct List_c{
     address_c first;
 };
 
-address_c allocate(infotype_c x);
 void createList(List_c &L);
-void insertJalan(List_c &L);
-void insertFirst(List_c &L, address_c P);
-void insertLast(List_c &L, address_c P);
-void insertAfter(List_c &L, address_c Prec, address_c P);
-void deleteJalan(List_c &L, address_c &P)
-void deleteFirst(List_c &L, address_c &P);
-void deleteLast(List_c &L, address_c &P);
-void deleteAfter(List_c &L, address_c Prec, address_c P);
+void insertJalan(List_c &L, infotype_c x, infotype_c tipe);
+void deleteJalan(List_c &L, address_c &P);
 void printInfo(List_c L);
 
 
