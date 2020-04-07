91aa44c4fb54f3e7db177e5f5a3c2bd758703323
rizqiramadhannn <rizqiramadhannn@users.noreply.github.com>
2020-04-07 16:24:58 +0700

    Update main.cpp

--- tubes/main.cpp
+++ tubes/main.cpp
@@ -14,13 +14,12 @@ int main()
         cout << "Menu" << endl;
         cout << "1. Tambah data kota" << endl;
         cout << "2. Tambah data jalan" << endl;
-        cout << "3. ?" << endl;
-        cout << "4. Hapus data kota" << endl;
-        cout << "5. Hapus data jalan" << endl;
-        cout << "6. Tampilkan semua data" << endl;
-        cout << "7. Tampilkan data kota berdasarkan jalan" << endl;
-        cout << "8. Tampilkan data jalan berdasarkan kota" << endl;
-        cout << "9. Tampilkan data jalan berdasarkan tipe jalan" << endl;
+        cout << "3. Hapus data kota" << endl;
+        cout << "4. Hapus data jalan" << endl;
+        cout << "5. Tampilkan semua data" << endl;
+        cout << "6. Tampilkan data kota berdasarkan jalan" << endl;
+        cout << "7. Tampilkan data jalan berdasarkan kota" << endl;
+        cout << "8. Tampilkan data jalan berdasarkan tipe jalan" << endl;
         cout << "0. End Program" << endl;
         cout << "---------------------------" << endl;
         cout << "Pilih menu : ";
@@ -44,33 +43,44 @@ int main()
             cin >> y;
             insertJalan(child(P), x, y);
         } else if (input == "3"){
-            cout << "Belum kelarrr" << endl;
-        } else if (input == "4"){
             deleteKota(Kota);
+        } else if (input == "4"){
+            if(first(Kota) == NULL){
+                cout << "Tidak ada data kota" << endl;
+            } else {
+                do{
+                    printKota(Kota);
+                    cout << "Masukkan nama kota : ";
+                    cin >> x;
+                    P = cariKota(Kota, x);
+                    if (P == NULL){
+                        cout << "Kota tidak ditemukan" << endl;
+                    }
+                } while (P == NULL);
+                printInfo(child(P));
+                deleteJalan(child(P), Q);
+            }
         } else if (input == "5"){
-            do{
-                printKota(Kota);
-                cout << "Masukkan nama kota : ";
-                cin >> x;
-                P = cariKota(Kota, x);
-                if (P == NULL){
-                    cout << "Kota tidak ditemukan" << endl;
-                }
-            } while (P == NULL);
-            printInfo(child(P));
-            deleteJalan(child(P), Q);
-        } else if (input == "6"){
             printAll(Kota);
+        } else if (input == "6"){
+            cout << "Belum kelarrr" << endl;
         } else if (input == "7"){
-            cout << "Belum kelarrr" << endl;
+            if(first(Kota) == NULL){
+                cout << "Tidak ada data kota" << endl;
+            } else {
+                do{
+                    printKota(Kota);
+                    cout << "Masukkan nama kota : ";
+                    cin >> x;
+                    P = cariKota(Kota, x);
+                    if (P == NULL){
+                        cout << "Kota tidak ditemukan" << endl;
+                    }
+                } while (P == NULL);
+                printInfo(child(P));
+            }
         } else if (input == "8"){
-            printKota(Kota);
-            cout << "Masukkan nama kota : ";
-            cin >> x;
-            P = cariKota(Kota, x);
-            printInfo(child(P));
-        } else if (input == "9"){
-            cout << "Belum kelarrr" << endl;
+             cout << "Belum kelarrr" << endl;
         } else if (input != "0"){
             cout << "Input salah/menu tidak tersedia" << endl;
         }
